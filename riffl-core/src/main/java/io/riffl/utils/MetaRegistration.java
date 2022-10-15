d9fe137a6b5b0c410d9389a509727c608d984b79
attzy <attzy@pm.me>
2022-10-15 17:12:01 +0100

    Overrides should also be applied to Catalogs and Databases

--- riffl-core/src/main/java/io/riffl/utils/MetaRegistration.java
+++ riffl-core/src/main/java/io/riffl/utils/MetaRegistration.java
@@ -1,13 +1,15 @@
 package io.riffl.utils;
 
 import io.riffl.config.ConfigUtils;
+import java.util.Properties;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 public class MetaRegistration {
 
-  public static void register(StreamTableEnvironment tableEnv, String stmtUri) {
+  public static void register(
+      StreamTableEnvironment tableEnv, String stmtUri, Properties overrides) {
     Path definitionPath = new Path(stmtUri);
-    tableEnv.executeSql(ConfigUtils.openFileAsString(definitionPath));
+    tableEnv.executeSql(ConfigUtils.openFileAsString(definitionPath, overrides));
   }
 }
