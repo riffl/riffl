b979a460f425a4c727d58d017149824e1fd37317
attzy <attzy@pm.me>
2022-12-14 10:31:22 +0000

    Upgrade Flink to 1.15.2

--- riffl-core/src/main/java/io/riffl/utils/TableHelper.java
+++ riffl-core/src/main/java/io/riffl/utils/TableHelper.java
@@ -4,44 +4,51 @@ import io.riffl.Riffl;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.api.bridge.java.internal.StreamTableEnvironmentImpl;
 import org.apache.flink.table.catalog.CatalogManager;
 import org.apache.flink.table.catalog.FunctionCatalog;
 import org.apache.flink.table.catalog.GenericInMemoryCatalog;
 import org.apache.flink.table.catalog.ObjectIdentifier;
+import org.apache.flink.table.delegation.Executor;
+import org.apache.flink.table.delegation.Planner;
+import org.apache.flink.table.factories.PlannerFactoryUtil;
 import org.apache.flink.table.module.ModuleManager;
 import org.apache.flink.table.operations.ddl.CreateTableOperation;
-import org.apache.flink.table.planner.delegation.DefaultExecutor;
-import org.apache.flink.table.planner.delegation.StreamPlanner;
 
 public class TableHelper {
 
   public static ObjectIdentifier getCreateTableIdentifier(
       String stmt, StreamExecutionEnvironment env, StreamTableEnvironment tableEnv) {
 
-    StreamPlanner planner = getStreamPlanner(env, tableEnv);
+    Planner planner = getStreamPlanner(env, tableEnv);
 
     CreateTableOperation operation = (CreateTableOperation) planner.getParser().parse(stmt).get(0);
 
     return operation.getTableIdentifier();
   }
 
-  private static StreamPlanner getStreamPlanner(
+  private static Planner getStreamPlanner(
       StreamExecutionEnvironment env, StreamTableEnvironment tableEnv) {
+
     CatalogManager catalogManager =
         CatalogManager.newBuilder()
             .classLoader(Riffl.class.getClassLoader())
             .config(tableEnv.getConfig().getConfiguration())
             .defaultCatalog(
-                EnvironmentSettings.DEFAULT_BUILTIN_CATALOG,
+                EnvironmentSettings.inStreamingMode().getBuiltInCatalogName(),
                 new GenericInMemoryCatalog(
-                    EnvironmentSettings.DEFAULT_BUILTIN_CATALOG,
-                    EnvironmentSettings.DEFAULT_BUILTIN_DATABASE))
+                    EnvironmentSettings.inStreamingMode().getBuiltInCatalogName(),
+                    EnvironmentSettings.inStreamingMode().getBuiltInDatabaseName()))
             .build();
+    final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
-    return new StreamPlanner(
-        new DefaultExecutor(env),
+    final Executor executor = StreamTableEnvironmentImpl.lookupExecutor(classLoader, env);
+
+    return PlannerFactoryUtil.createPlanner(
+        executor,
         tableEnv.getConfig(),
-        new FunctionCatalog(tableEnv.getConfig(), catalogManager, new ModuleManager()),
-        catalogManager);
+        new ModuleManager(),
+        catalogManager,
+        new FunctionCatalog(tableEnv.getConfig(), catalogManager, new ModuleManager()));
   }
 }
