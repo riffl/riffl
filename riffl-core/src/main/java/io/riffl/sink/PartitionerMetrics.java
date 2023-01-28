4f14a742f6ccad872451345580d122aee5183637
attzy <attzy@pm.me>
2023-01-28 05:00:53 +0000

    Move task allocation and metrics setup into initializeState stage

--- riffl-core/src/main/java/io/riffl/sink/PartitionerMetrics.java
+++ riffl-core/src/main/java/io/riffl/sink/PartitionerMetrics.java
@@ -11,7 +11,6 @@ import io.riffl.sink.row.TasksAssignment;
 import io.riffl.sink.row.tasks.TaskAssigner;
 import io.riffl.sink.row.tasks.TaskAssignerMetricsFactory;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.configuration.Configuration;
 import org.apache.flink.runtime.state.FunctionInitializationContext;
 import org.apache.flink.runtime.state.FunctionSnapshotContext;
 import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
@@ -19,9 +18,13 @@ import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.types.Row;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PartitionerMetrics extends ProcessFunction<Row, Tuple2<Row, Integer>>
     implements CheckpointedFunction {
+
+  private static final Logger logger = LoggerFactory.getLogger(PartitionerMetrics.class);
 
   private final Sink sink;
   private final MetricsStore metricsStore;
@@ -46,16 +49,6 @@ public class PartitionerMetrics extends ProcessFunction<Row, Tuple2<Row, Integer
   }
 
   @Override
-  public void open(Configuration parameters) {
-    this.metricsProcessor = new DefaultMetricsProcessor(sink, metricsStore);
-    this.taskAllocation.configure();
-    this.tasksAssignment = new TasksAssignment();
-    this.taskAssigner =
-        taskAssignerFactory.create(
-            sink, taskAllocation.getSinkTasks(sink), tasksAssignment, new Metrics());
-  }
-
-  @Override
   public void snapshotState(FunctionSnapshotContext context) {
     var checkpointId = context.getCheckpointId();
     metricsProcessor.flushMetrics(checkpointId);
@@ -69,13 +62,21 @@ public class PartitionerMetrics extends ProcessFunction<Row, Tuple2<Row, Integer
 
   @Override
   public void initializeState(FunctionInitializationContext context) {
+    this.taskAllocation.configure();
+    this.tasksAssignment = new TasksAssignment();
+    this.metricsProcessor = new DefaultMetricsProcessor(sink, metricsStore);
     if (context.isRestored()) {
+      logger.info("context.getRestoredCheckpointId():" + context.getRestoredCheckpointId());
       this.taskAssigner =
           taskAssignerFactory.create(
               sink,
               taskAllocation.getSinkTasks(sink),
               tasksAssignment,
               metricsProcessor.getMetrics(context.getRestoredCheckpointId().orElse(0)));
+    } else {
+      this.taskAssigner =
+          taskAssignerFactory.create(
+              sink, taskAllocation.getSinkTasks(sink), tasksAssignment, new Metrics());
     }
   }
 
